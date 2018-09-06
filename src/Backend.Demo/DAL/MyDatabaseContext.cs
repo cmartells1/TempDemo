5eb7de3c054e6dfcd2f3f774eed0a5947da88a10
Chris Martells <42975195+cmartells1@users.noreply.github.com>
2018-09-06 14:46:18 -0600

    Datasource About Page

--- src/Backend.Demo/DAL/MyDatabaseContext.cs
+++ src/Backend.Demo/DAL/MyDatabaseContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Backend.Demo.Entites;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Backend.Demo.DAL
 {
-    class MyDatabaseContext
+    internal class MyDatabaseContext :DbContext
     {
+        public MyDatabaseContext() : base("name=AdHocDb")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
